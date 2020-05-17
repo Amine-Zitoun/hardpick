e483f063a595f35a0491a76d818b972c998acc86
RealSharonX <racemaloui88@gmail.com>
2020-05-17 16:34:04 +0100

    done ?

--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+let winner = {
+    pr: "n/a",
+    name: "n/a",
+    price: 0,
+    site: "n/a",
+    unit: "n/a",
+    differnce: 0
+}
+
+
+let currencies = {
+    tunisia: "TND"
+}
+
 
 let recieved = false;
 
@@ -21,31 +35,42 @@ router.post("/", (req, res) => {
         res.render("index.ejs", {errorMessage: "Please select your country"})
     } else if (!price) {
         res.render("index.ejs", {errorMessage: "Please enter your budget"})
+    } else if (price < 100){
+        res.render("index.ejs", {errorMessage: `Budget too low (under 100${currencies[region]})`})
     } else {
         switch (region) {
             case "tunisia":
                 newPrice = price * 1000;
                 break;
         }
-        getData(comp, newPrice, res);
+        getData(comp, newPrice, res, currencies[region]);
 
         return
     }
 })
 
+router.get("/showWin", (req, res) => {
+    res.json(winner);
+})
+
 router.get("/end", (req, res) => {
     recieved = false;
-    res.json(recieved);
     console.log(recieved);
 })
 
-function getData(comp, newPrice, res) {
+function getData(comp, newPrice, res, unit) {
     const api = `https://alphapicker.herokuapp.com/api/?comp=${comp}&budget=${newPrice}`;
     axios.get(api)
         .then((response) => {
             recieved = true;
             const data = response.data;
             console.log(data);
+            winner.name = data.win.prd;
+            winner.pr = data.win.pr;
+            winner.price = data.win.price;
+            winner.site = data.win.site;
+            winner.unit = unit;
+            winner.differnce = winner.price - newPrice;
             console.log(recieved)
             res.status(204).send()
         })
